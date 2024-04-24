cd88b10574136a577923296ec05d720d43dd37fe
Pratish Ninawe <pratish.1006@gmail.com>
2024-04-24 17:16:15 +0530

    Feat: added vercel analytics
    Signed-off-by: Pratish Ninawe <pratish.1006@gmail.com>

--- Client/src/main.jsx
+++ Client/src/main.jsx
@@ -6,17 +6,20 @@ import { Provider } from "react-redux";
 import { Toaster } from "react-hot-toast";
 import { PersistGate } from "redux-persist/integration/react";
 import { persistStore } from "redux-persist";
+import { Analytics } from "@vercel/analytics/react"
 
 const persistor = persistStore(store);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <Provider store={store}>
-    <PersistGate persistor={persistor}>
-      <App />
-      <Toaster
-        position="top-right"
-        toastOptions={{ style: { fontFamily: "Pixelify Sans" } }}
-      />
-    </PersistGate>
-  </Provider>
+  <Analytics>
+    <Provider store={store}>
+      <PersistGate persistor={persistor}>
+        <App />
+        <Toaster
+          position="top-right"
+          toastOptions={{ style: { fontFamily: "Pixelify Sans" } }}
+        />
+      </PersistGate>
+    </Provider>
+  </Analytics>
 );
